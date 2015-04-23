66f0361f370f17835efd419dfb43e2abc7519982
AdComte <adrien.comte.69@gmail.com>
2015-04-23 10:47:42 +0200

    images lists

--- stupvau/Form1.Designer.cs
+++ stupvau/Form1.Designer.cs
@@ -29,11 +29,160 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cardsplayer1 = new System.Windows.Forms.ImageList(this.components);
+            this.cardsplayer2 = new System.Windows.Forms.ImageList(this.components);
+            this.cardsplayer3 = new System.Windows.Forms.ImageList(this.components);
+            this.cardsplayer4 = new System.Windows.Forms.ImageList(this.components);
+            this.cardsplayer5 = new System.Windows.Forms.ImageList(this.components);
+            this.animalcards = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // cardsplayer1
+            // 
+            this.cardsplayer1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cardsplayer1.ImageStream")));
+            this.cardsplayer1.TransparentColor = System.Drawing.Color.Transparent;
+            this.cardsplayer1.Images.SetKeyName(0, "00.png");
+            this.cardsplayer1.Images.SetKeyName(1, "01.png");
+            this.cardsplayer1.Images.SetKeyName(2, "02.png");
+            this.cardsplayer1.Images.SetKeyName(3, "03.png");
+            this.cardsplayer1.Images.SetKeyName(4, "04.png");
+            this.cardsplayer1.Images.SetKeyName(5, "05.png");
+            this.cardsplayer1.Images.SetKeyName(6, "06.png");
+            this.cardsplayer1.Images.SetKeyName(7, "07.png");
+            this.cardsplayer1.Images.SetKeyName(8, "08.png");
+            this.cardsplayer1.Images.SetKeyName(9, "09.png");
+            this.cardsplayer1.Images.SetKeyName(10, "10.png");
+            this.cardsplayer1.Images.SetKeyName(11, "11.png");
+            this.cardsplayer1.Images.SetKeyName(12, "12.png");
+            this.cardsplayer1.Images.SetKeyName(13, "13.png");
+            this.cardsplayer1.Images.SetKeyName(14, "14.png");
+            this.cardsplayer1.Images.SetKeyName(15, "15.png");
+            // 
+            // cardsplayer2
+            // 
+            this.cardsplayer2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cardsplayer2.ImageStream")));
+            this.cardsplayer2.TransparentColor = System.Drawing.Color.Transparent;
+            this.cardsplayer2.Images.SetKeyName(0, "00.png");
+            this.cardsplayer2.Images.SetKeyName(1, "01.png");
+            this.cardsplayer2.Images.SetKeyName(2, "02.png");
+            this.cardsplayer2.Images.SetKeyName(3, "03.png");
+            this.cardsplayer2.Images.SetKeyName(4, "04.png");
+            this.cardsplayer2.Images.SetKeyName(5, "05.png");
+            this.cardsplayer2.Images.SetKeyName(6, "06.png");
+            this.cardsplayer2.Images.SetKeyName(7, "07.png");
+            this.cardsplayer2.Images.SetKeyName(8, "08.png");
+            this.cardsplayer2.Images.SetKeyName(9, "09.png");
+            this.cardsplayer2.Images.SetKeyName(10, "10.png");
+            this.cardsplayer2.Images.SetKeyName(11, "11.png");
+            this.cardsplayer2.Images.SetKeyName(12, "12.png");
+            this.cardsplayer2.Images.SetKeyName(13, "13.png");
+            this.cardsplayer2.Images.SetKeyName(14, "14.png");
+            this.cardsplayer2.Images.SetKeyName(15, "15.png");
+            // 
+            // cardsplayer3
+            // 
+            this.cardsplayer3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cardsplayer3.ImageStream")));
+            this.cardsplayer3.TransparentColor = System.Drawing.Color.Transparent;
+            this.cardsplayer3.Images.SetKeyName(0, "00.png");
+            this.cardsplayer3.Images.SetKeyName(1, "01.png");
+            this.cardsplayer3.Images.SetKeyName(2, "02.png");
+            this.cardsplayer3.Images.SetKeyName(3, "03.png");
+            this.cardsplayer3.Images.SetKeyName(4, "04.png");
+            this.cardsplayer3.Images.SetKeyName(5, "05.png");
+            this.cardsplayer3.Images.SetKeyName(6, "06.png");
+            this.cardsplayer3.Images.SetKeyName(7, "07.png");
+            this.cardsplayer3.Images.SetKeyName(8, "08.png");
+            this.cardsplayer3.Images.SetKeyName(9, "09.png");
+            this.cardsplayer3.Images.SetKeyName(10, "10.png");
+            this.cardsplayer3.Images.SetKeyName(11, "11.png");
+            this.cardsplayer3.Images.SetKeyName(12, "12.png");
+            this.cardsplayer3.Images.SetKeyName(13, "13.png");
+            this.cardsplayer3.Images.SetKeyName(14, "14.png");
+            this.cardsplayer3.Images.SetKeyName(15, "15.png");
+            // 
+            // cardsplayer4
+            // 
+            this.cardsplayer4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cardsplayer4.ImageStream")));
+            this.cardsplayer4.TransparentColor = System.Drawing.Color.Transparent;
+            this.cardsplayer4.Images.SetKeyName(0, "00.png");
+            this.cardsplayer4.Images.SetKeyName(1, "01.png");
+            this.cardsplayer4.Images.SetKeyName(2, "02.png");
+            this.cardsplayer4.Images.SetKeyName(3, "03.png");
+            this.cardsplayer4.Images.SetKeyName(4, "04.png");
+            this.cardsplayer4.Images.SetKeyName(5, "05.png");
+            this.cardsplayer4.Images.SetKeyName(6, "06.png");
+            this.cardsplayer4.Images.SetKeyName(7, "07.png");
+            this.cardsplayer4.Images.SetKeyName(8, "08.png");
+            this.cardsplayer4.Images.SetKeyName(9, "09.png");
+            this.cardsplayer4.Images.SetKeyName(10, "10.png");
+            this.cardsplayer4.Images.SetKeyName(11, "11.png");
+            this.cardsplayer4.Images.SetKeyName(12, "12.png");
+            this.cardsplayer4.Images.SetKeyName(13, "13.png");
+            this.cardsplayer4.Images.SetKeyName(14, "14.png");
+            this.cardsplayer4.Images.SetKeyName(15, "15.png");
+            // 
+            // cardsplayer5
+            // 
+            this.cardsplayer5.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("cardsplayer5.ImageStream")));
+            this.cardsplayer5.TransparentColor = System.Drawing.Color.Transparent;
+            this.cardsplayer5.Images.SetKeyName(0, "00.png");
+            this.cardsplayer5.Images.SetKeyName(1, "01.png");
+            this.cardsplayer5.Images.SetKeyName(2, "02.png");
+            this.cardsplayer5.Images.SetKeyName(3, "03.png");
+            this.cardsplayer5.Images.SetKeyName(4, "04.png");
+            this.cardsplayer5.Images.SetKeyName(5, "05.png");
+            this.cardsplayer5.Images.SetKeyName(6, "06.png");
+            this.cardsplayer5.Images.SetKeyName(7, "07.png");
+            this.cardsplayer5.Images.SetKeyName(8, "08.png");
+            this.cardsplayer5.Images.SetKeyName(9, "09.png");
+            this.cardsplayer5.Images.SetKeyName(10, "10.png");
+            this.cardsplayer5.Images.SetKeyName(11, "11.png");
+            this.cardsplayer5.Images.SetKeyName(12, "12.png");
+            this.cardsplayer5.Images.SetKeyName(13, "13.png");
+            this.cardsplayer5.Images.SetKeyName(14, "14.png");
+            this.cardsplayer5.Images.SetKeyName(15, "15.png");
+            // 
+            // animalcards
+            // 
+            this.animalcards.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("animalcards.ImageStream")));
+            this.animalcards.TransparentColor = System.Drawing.Color.Transparent;
+            this.animalcards.Images.SetKeyName(0, "00.png");
+            this.animalcards.Images.SetKeyName(1, "01.png");
+            this.animalcards.Images.SetKeyName(2, "02.png");
+            this.animalcards.Images.SetKeyName(3, "03.png");
+            this.animalcards.Images.SetKeyName(4, "04.png");
+            this.animalcards.Images.SetKeyName(5, "05.png");
+            this.animalcards.Images.SetKeyName(6, "06.png");
+            this.animalcards.Images.SetKeyName(7, "07.png");
+            this.animalcards.Images.SetKeyName(8, "08.png");
+            this.animalcards.Images.SetKeyName(9, "09.png");
+            this.animalcards.Images.SetKeyName(10, "10.png");
+            this.animalcards.Images.SetKeyName(11, "11.png");
+            this.animalcards.Images.SetKeyName(12, "12.png");
+            this.animalcards.Images.SetKeyName(13, "13.png");
+            this.animalcards.Images.SetKeyName(14, "14.png");
+            this.animalcards.Images.SetKeyName(15, "15.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 410);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList cardsplayer1;
+        private System.Windows.Forms.ImageList cardsplayer2;
+        private System.Windows.Forms.ImageList cardsplayer3;
+        private System.Windows.Forms.ImageList cardsplayer4;
+        private System.Windows.Forms.ImageList cardsplayer5;
+        private System.Windows.Forms.ImageList animalcards;
     }
 }
 

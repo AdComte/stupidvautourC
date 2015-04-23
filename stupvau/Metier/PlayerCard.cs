ccbcdc952d0e027f9cd0d5ff3da87194993067e6
vladmourier <vladmourier@hotmail.fr>
2015-04-23 10:14:46 +0200

    Début traduction

---
+++ stupvau/Metier/PlayerCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace stupvau.Metier
+{
+    class PlayerCard
+    {
+
+        private int couleur;
+
+        public PlayerCard(int number, bool notPlayed, int couleur) : base()
+        {
+           // super(number, notPlayed);
+            this.couleur = couleur;
+        }
+
+        public int getCouleur()
+        {
+            return couleur;
+        }
+    
+    }
+}

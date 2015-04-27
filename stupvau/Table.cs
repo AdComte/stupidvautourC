4696c6152680ad4e9a65a617454e95a51405faa6
AdComte <adrien.comte.69@gmail.com>
2015-04-28 00:28:30 +0200

    nouveau constructeur, meilleur verrou sur le bouton

--- stupvau/Table.cs
+++ stupvau/Table.cs
@@ -13,8 +13,9 @@ namespace stupvau
     {
         public bool[] playablecards;
         public int selectedcard;
+        public int nbplayer;
 
-        public Table()
+        private Table()
         {
             InitializeComponent();
             for(int i=0; i < 15; i++)
@@ -24,6 +25,30 @@ namespace stupvau
             selectedcard = 0;
             pb_player.Image = cardsplayer1.Images[0];
         }
+
+        public Table(int nbplayer) : this()
+        {
+            this.nbplayer = nbplayer;
+            if (nbplayer <= 4)
+            {
+                pb_player5.Visible = false;
+                lbl_player5name.Visible = false;
+                lbl_player5points.Visible = false;
+            }
+            if (nbplayer <= 3)
+            {
+                pb_player4.Visible = false;
+                lbl_player4name.Visible = false;
+                lbl_player4points.Visible = false;
+            }
+            if (nbplayer <= 2)
+            {
+                pb_player3.Visible = false;
+                lbl_player3name.Visible = false;
+                lbl_player3points.Visible = false;
+            }
+        }
+
 
         private void lockgame() //Rend les toutes cartes in sélectionnables et le bouton incliquable
         {
@@ -43,20 +68,31 @@ namespace stupvau
                 {
                     c.Enabled = true;
                 }
+                else
+                {
+                    c.Visible = false;
+                }
+                i++;
             }
             btn_valid.Enabled = true;
+            selectedcard = 0;
+            pb_player.Image = cardsplayer1.Images[selectedcard];
         }
 
         private void btn_valid_Click(object sender, EventArgs e)
         {
             //TODO, on valide le coup, redone la main à la boucle de jeu ne pas oublier d'ajouter la carte jouée dans payablecards false
-            lockgame();
+            if (selectedcard != 0) //IMPORTANT
+            {
+                lockgame();
+                playablecards[selectedcard] = false;
+            }
         }
 
         private void pb_click(object sender, EventArgs e)
         {
             Control card = (Control)sender;
-            selectedcard = card.TabIndex + 1;
+            selectedcard = card.TabIndex;
             pb_player.Image = cardsplayer1.Images[selectedcard];
         }
     }

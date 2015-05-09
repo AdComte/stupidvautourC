96400b8f69921b3c9c0b76b0807ed66ffc73d9af
SylvainBeg <sylvain.beg@gmail.com>
2015-05-10 00:21:32 +0200

    Résolution bug première carte. + autres

--- stupvau/Metier/Table.cs
+++ stupvau/Metier/Table.cs
@@ -34,38 +34,6 @@ namespace stupvau.Metier
 			return listPlayer;
 		}
 
-		// Fait jouer chaque joueur et met leur carte sur la table
-		// Return : Tous les joueurs ont joué une carte sur la table
-		//public void play() {
-        //    Console.WriteLine("Carte du stack : "); /////////////////
-        //    Console.Write(this.current.getValue() + " | ");     //////////////
-        //    Console.Write(this.current.getAnimal() + " \n");     //////////////
-		//	int point = this.getCurrent().getValue();
-		//	if (this.getCurrent().getAnimal()) {
-        //    point = point * -1;
-		//	}
-        //
-        //
-		//	 Console.WriteLine("************************");        ///////////
-		//	 foreach (Player p in this.listPlayer)
-		//	 {
-		//		 p.affichecartes(p);
-		//			PlayerCard a = p.play(this);
-		//			Console.WriteLine("Valeur carte joueur"+ p.getCouleur()+" :");        ///////////
-		//			Console.WriteLine(a.getValue());                   ///////////
-		//			//this.listPlayerCardsOnTable.Add(a);
-		//	 }
-		//
-		//	 int winnerRound = this.win_round();
-		//		if (winnerRound >= 0) 
-		//		{
-		//			this.listPlayer.ElementAt(winnerRound).setScore(point);
-		//			Console.WriteLine("Score mis à jour: "); /////////////////
-		//			Console.WriteLine(this.listPlayer[winnerRound].getScore()); /////////////
-		//		}
-		//		else if (winnerRound == -1) { Console.WriteLine("ERREUR : Personne n'a gagné ce round, il n'y a pas égalité non plus"); }
-		//		else if (winnerRound == -2) { Console.WriteLine("Personne n'a gagné ce tour ci, les cartes seront défaussées"); }
-		//}
 
 		//Retourne le numéro du joueur au plus haut score
 		public int getPlayerHighestScore() {
@@ -79,15 +47,6 @@ namespace stupvau.Metier
 			return winner;
 		}
 
-		public void deal() {
-			for (int i = 1; i < NB_VULTURE + 1; i++) {
-				this.stack.Add(new AnimalCard(i, true));
-			}
-			for (int i = 1; i < NB_MOUSE + 1; i++) {
-				this.stack.Add(new AnimalCard(i, false));
-			}
-		}
-
 		public void melangerAnimalCard() {
 
 			IList<AnimalCard> pioche = new List<AnimalCard>();
@@ -98,7 +57,6 @@ namespace stupvau.Metier
 			for (int i = 1; i < NB_MOUSE + 1; i++) {
 				pioche.Add(new AnimalCard(i, false));
 			}
-			//Console.WriteLine("Au commencement, la pile contient : " + this.stack.Count() + " éléments");
 			Random j = new Random();
 
 
@@ -110,18 +68,8 @@ namespace stupvau.Metier
 			}
 			this.stack = stackMelanger;
 			current = stack[0];
-
-			//foreach(AnimalCard a in this.stack)
-			//{
-			//    if(a.getAnimal())
-			//    {
-			//        Console.WriteLine("Vautour/" + a.getValue());
-			//    }
-			//    else
-			//    {
-			//        Console.WriteLine("Souris/" + a.getValue());
-			//    }
-			//}
+            Console.WriteLine("Au commencement, la pile contient : " + this.stack.Count() + " éléments");
+            stack.RemoveAt(0);
 		}
 
 		//TODO : cas d'égalité
@@ -172,6 +120,7 @@ namespace stupvau.Metier
 			Console.WriteLine("La table contient : "+ listCardGagnantes.Count + " Cartes gagnantes");
 			if (listCardGagnantes.Count == 1) {
 				Console.WriteLine("Le joueur" + listCardGagnantes[0].getCouleur() + "gagne le round");
+                Console.WriteLine(this.getStack().Count); Console.WriteLine("MENGLON !!!");
 				return listCardGagnantes.ElementAt(0).getCouleur();
 			} else if (listCardGagnantes.Count == 0) {
 				return -1;
